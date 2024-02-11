2a68642bb72db689da896a6f4590b577ba1a64ce
Aneta Kasprzak <anet.kasprzak@gmail.com>
2024-02-11 15:19:51 +0000

    refactor structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 export default function App() {
   return (
-    <picture className="hero__img">
-      <source
-        srcSet="../public/images/image-hero-desktop.jpg"
-        media="(min-width: 90rem)"
-      />
-      <source srcSet="../public/images/image-hero-mobile.jpg" />
-      <img src="../public/images/image-hero-mobile.jpg" alt="hero image" />
-    </picture>
+    <div className="wrapper">
+      <header>
+        <nav>
+          <div className="logo">
+            <img src="../public/images/logo.svg" alt="logo" />
+          </div>
+          <div className="hamburger">HAM</div>
+        </nav>
+      </header>
+    </div>
   );
 }
