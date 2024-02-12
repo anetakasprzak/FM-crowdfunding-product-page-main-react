4ccf3cb5ae4de1e4c7d617ed4ee1d6b5e76a7cf6
Aneta Kasprzak <anet.kasprzak@gmail.com>
2024-02-12 17:10:54 +0000

    main section for mobile and desktop

--- src/App.jsx
+++ src/App.jsx
@@ -10,25 +10,28 @@ export default function App() {
         </nav>
       </header>
 
-      <section className="main__section">
-        <img
-          src="../public/images/logo-mastercraft.svg"
-          alt="logo"
-          className="logo--icon"
-        />
-        <h2 className="heading">Mastercraft Bamboo Monitor Riser</h2>
-        <p className="text">
-          A beautifully handcrafted monitor stand to reduce neck and eye strain.
-        </p>
-        <div className="btns__box">
-          <button className="btn btn__main">Back this project</button>
-          <button className="btn__bookmark">
-            <img
-              src="../public/images/icon-bookmark.svg"
-              alt="bookmark icon"
-              className="bookmark--icon"
-            />
-          </button>
+      <section className="main__content">
+        <div className="cta__section section__wrapper">
+          <img
+            src="../public/images/logo-mastercraft.svg"
+            alt="logo"
+            className="logo--icon"
+          />
+          <h2 className="heading--cta">Mastercraft Bamboo Monitor Riser</h2>
+          <p className="text text--cta">
+            A beautifully handcrafted monitor stand to reduce neck and eye
+            strain.
+          </p>
+          <div className="btns__box">
+            <button className="btn btn__main">Back this project</button>
+            <button className="btn__bookmark">
+              <img
+                src="../public/images/icon-bookmark.svg"
+                alt="bookmark icon"
+                className="bookmark--icon"
+              />
+            </button>
+          </div>
         </div>
       </section>
     </div>
