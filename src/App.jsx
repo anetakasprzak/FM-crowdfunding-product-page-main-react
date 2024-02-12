a2a649ee881a3bf5bed6cd8b27c568fb2ec15dd8
Aneta Kasprzak <anet.kasprzak@gmail.com>
2024-02-12 14:31:48 +0000

    main content for mobile

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,36 @@
 export default function App() {
   return (
     <div className="wrapper">
-      <header>
-        <nav>
+      <header className="header">
+        <nav className="nav">
           <div className="logo">
             <img src="../public/images/logo.svg" alt="logo" />
           </div>
           <div className="hamburger">HAM</div>
         </nav>
       </header>
+
+      <section className="main__section">
+        <img
+          src="../public/images/logo-mastercraft.svg"
+          alt="logo"
+          className="logo--icon"
+        />
+        <h2 className="heading">Mastercraft Bamboo Monitor Riser</h2>
+        <p className="text">
+          A beautifully handcrafted monitor stand to reduce neck and eye strain.
+        </p>
+        <div className="btns__box">
+          <button className="btn btn__main">Back this project</button>
+          <button className="btn__bookmark">
+            <img
+              src="../public/images/icon-bookmark.svg"
+              alt="bookmark icon"
+              className="bookmark--icon"
+            />
+          </button>
+        </div>
+      </section>
     </div>
   );
 }
