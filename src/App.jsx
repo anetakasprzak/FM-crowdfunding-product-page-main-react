e7ab746c39724ee9b97def5bbd0ea8f972361eb9
Aneta Kasprzak <anet.kasprzak@gmail.com>
2024-02-13 20:00:42 +0000

    about section - mobile and desktop

--- src/App.jsx
+++ src/App.jsx
@@ -37,7 +37,7 @@ export default function App() {
           </div>
         </div>
 
-        <div className="stats__section section__wrapper">
+        <section className="stats__section section__wrapper">
           <div className="stats">
             <div className="stats__box">
               <p className="number__big">$89,914</p>
@@ -58,7 +58,43 @@ export default function App() {
           <div className="stats__line">
             <span className="green"></span>
           </div>
-        </div>
+        </section>
+
+        <section className="about__section section__wrapper">
+          <div className="about__text-box">
+            <h3 className="about__heading">About this project</h3>
+            <p className="text about__text">
+              The Mastercraft Bamboo Monitor Riser is a sturdy and stylish
+              platform that elevates your screen to a more comfortable viewing
+              height. Placing your monitor at eye level has the potential to
+              improve your posture and make you more comfortable while at work,
+              helping you stay focused on the task at hand.
+              <br />
+              <br /> Featuring artisan craftsmanship, the simplicity of design
+              creates extra desk space below your computer to allow notepads,
+              pens, and USB sticks to be stored under the stand.
+            </p>
+          </div>
+
+          <div className="reward">
+            <div className="reward__heading--box">
+              <h4 className="reward__heading">Bamboo Stand</h4>
+              <p className="pledge__heading">Pledge $25 or more</p>
+            </div>
+            <p className="text reward__text">
+              You get an ergonomic stand made of natural bamboo. You&apos;ve
+              helped us launch our promotional campaign, and you&apos;ll be
+              added to a special Backer member list.
+            </p>
+            <div className="btn__left--box">
+              <div className="left__box">
+                <p className="reward__number">101</p>
+                <p className="text">left</p>
+              </div>
+              <button className="btn btn__reward">Select Reward</button>
+            </div>
+          </div>
+        </section>
       </section>
     </div>
   );
